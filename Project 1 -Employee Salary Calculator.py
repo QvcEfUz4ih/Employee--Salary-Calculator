21e0f0022c7d5a45d1018fe238c20d9bd860385e
QvcEfUz4ih <nontopearl21@gmail.com>
2026-07-15 22:33:50 +0200

    Add files via upload

---
+++ Project 1 -Employee Salary Calculator.py
@@ -0,0 +1,73 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/nonto/OneDrive/Desktop/Python Projects/Salary_Calculator.py
+=====================================
+Employee Salary Calculator
+=====================================
+Enter employee name : Pearl
+Enter hours worked: 160
+Enter hourly rate: 120
+
+= RESTART: C:/Users/nonto/OneDrive/Desktop/Python Projects/Salary_Calculator.py
+=====================================
+Employee Salary Calculator
+=====================================
+Enter employee name:Pearl
+Enter hours worked: 160
+Enter hourly rate: 120
+
+Salary Summary
+=====================================
+Employee: :Pearl
+Hours Worked: 160.0
+Hourly Rate: R 120.0
+Traceback (most recent call last):
+  File "C:/Users/nonto/OneDrive/Desktop/Python Projects/Salary_Calculator.py", line 16, in <module>
+    print("Monthly Salary: R", salary)
+NameError: name 'salary' is not defined. Did you mean: 'Salary'?
+
+= RESTART: C:/Users/nonto/OneDrive/Desktop/Python Projects/Salary_Calculator.py
+=====================================
+Employee Salary Calculator
+=====================================
+Enter employee name: Pearl
+Enter hours worked: 160
+Enter hourly rate: 120
+
+Salary Summary
+=====================================
+Employee: : Pearl
+Hours Worked: 160.0
+Hourly Rate: R 120.0
+Monthly Salary: R 19200.0
+>>> 
+= RESTART: C:/Users/nonto/OneDrive/Desktop/Python Projects/Salary_Calculator.py
+=====================================
+Employee Salary Calculator
+=====================================
+Enter employee name: Pearl
+Enter hours worked: 160
+Enter hourly rate: 120
+
+Salary Summary
+=====================================
+Employee: : Pearl
+Hours Worked: 160.0
+Hourly Rate: R 120.0
+Monthly Salary: R 19200.0
+>>> 
+= RESTART: C:/Users/nonto/OneDrive/Desktop/Python Projects/Salary_Calculator.py
+=====================================
+Employee Salary Calculator
+=====================================
+Enter employee name: Pearl
+Enter hours worked: 170
+Enter hourly rate: 120
+
+Salary Summary
+=====================================
+Employee: : Pearl
+Hours Worked: 170.0
+Hourly Rate: R 120.0
+Monthly Salary: R 21000.0
